a119cb9271ff6639e97d9eea9b692513fd99d4bf
Tyler D. Franklin <irishlep84@gmail.com>
2017-07-14 16:23:13 -0500

    Added ability to upgrade and downgrade account

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   resources :wikis
 
   devise_for :users
-  resources :users, only: [:show]
+  
+  resources :users, only: [:show] do
+    get :downgrade_account
+  end
+
+  resources :charges, only: [:new, :create]
 
   get 'welcome/index'
 
