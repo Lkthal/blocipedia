d04484f98cdf2c5c9d0e343d482deed0afc061b1
Tyler D. Franklin <irishlep84@gmail.com>
2017-07-20 09:50:42 -0500

    Collaborator functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :wikis
+  resources :collaborators
 
   devise_for :users
   
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   end
 
   resources :charges, only: [:new, :create]
+
+  resources :wikis do
+    resources :collaborators, only: [:new, :create, :destroy]
+  end
 
   get 'welcome/index'
 
